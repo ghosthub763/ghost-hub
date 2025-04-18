800e39f651e20c64316b408e53965856a129e9ea
ghosthub763 <ghostttks@gmail.com>
2025-04-18 00:03:05 -0300

    update script

---
+++ main.lua
@@ -0,0 +1,45 @@
+if game.PleaceId == 4058282580 then
+    
+
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+
+    local Window = OrionLib:MakeWindow({Name = "ghost hub", HidePremium = false, SaveConfig = true, ConfigFolder = "ghost", IntroEnable = false})
+
+    local main = Window:MakeTab({
+        Name = "auto farm",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
+    })
+
+    local Section = main:AddSection({
+        Name = "auto farm"
+    })
+
+    main:AddToggle({
+        Name = "autosell",
+        Default = false,
+        Callback = function(Value)
+        pcall(function ()
+            game:GetService("ReplicatedStorage").Events.SellRequest:FireServer()
+        end)
+        task.wait(0.1)
+        
+        end    
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
+
+
