3b0af697d5dc44765860acfded0ab3c8ee1ba209
ghosthub763 <ghostttks@gmail.com>
2025-04-18 12:05:03 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,31 +1,51 @@
-if game.PlaceId == 4058282580 then
+if game.PlaceId == 74260430392611 then
     
 
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 
-    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-
-    local Window = OrionLib:MakeWindow({Name = "Ghost hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Dev Ghost", IntroEnable = false})
-
+local Window = OrionLib:MakeWindow({Name = "Ghost hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Dev Ghost", IntroEnable = false})
 
 
-    local Main = Window:MakeTab({
-            Name = "Luna",
-            Icon = "rbxassetid://4483345998",
-            PremiumOnly = false
-    })
-    local Section = Main:AddSection({
-        Name = "Auto-farm"
-    })
+local Main = Window:MakeTab({
+    Name = "Luna",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
 
-    Main:AddToggle({
-        Name = "Auto-sell",
-        Default = false,
-        pcall = function(Value)
-            game:GetService("ReplicatedStorage").Events.SellRequest:FireServer()  
+local Section = Main:AddSection({
+    Name = "Auto-farm"
+})
+
+_G.loopAtivo = false
+
+Main:AddToggle({
+    Name = "Loop de Exemplo",
+    Default = false,
+    Callback = function(value)
+        _G.loopAtivo = value
+        while _G.loopAtivo do
+            print("Executando ação...")
+            task.wait(1)
+        end
+    end
+})
+
+
+main:AddToggle({
+    Name = "AutoClick",
+    Default = false,
+    Callback = function(state)
+        _G.click = state
+        while _G.click do
+            pcall(function()
+                game:GetService("ReplicatedStorage").Events.Click:FireServer()
+            end)
             task.wait(0.1)
+        end
+    end
+})
 
-        end    
-    })
+    
 end
 
 
